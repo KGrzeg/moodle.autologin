4fe5f8d097809444d69fd1e9eeed1fe2f054b478
KGrzeg <kupczykgrzeg@gmail.com>
2020-04-30 00:51:13 +0200

    Implemented stream hanging wireframe

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const yargs = require("yargs");
 const defaultLoginURL =
   "https://logowanie.pg.edu.pl/login?service=https%3A%2F%2Fenauczanie.pg.edu.pl%2Fmoodle%2Flogin%2Findex.php%3FauthCAS%3DCAS";
 
+const minimumHangTime = 60 * 1000; //60s
+const hangCheckInterval = 15 * 1000;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function parseArguments() {
   return yargs
     .command(
@@ -38,7 +45,7 @@ function parseArguments() {
             },
             fs: {
               alias: "fullscreen",
-              describe: "Turn on fullscreen mode",
+              describe: "Turn on fullscreen mode; If set, ignore width and height options",
               default: false,
               type: "boolean",
             },
@@ -55,21 +62,69 @@ async function login(page, url, username, password) {
   await page.click("#submit_button");
 }
 
-async function main() {
-  const args = parseArguments();
+async function endStreamCheck(page) {
+  return Math.random() > 0.9; //TODO
+}
 
-  const browser = await puppeteer.launch({
+async function hangUntilStreamEnd(page) {
+  return new Promise(async (resolve, reject) => {
+    await sleep(minimumHangTime);
+
+    if (await endStreamCheck(page))
+      resolve();
+
+    const clock = setInterval(async () => {
+      try {
+        if (await endStreamCheck(page)) {
+          clearInterval(clock);
+          resolve();
+        }
+      } catch (err) {
+        clearInterval(clock);
+        reject(err);
+      }
+    }, hangCheckInterval);
+  });
+}
+
+async function setupBrowser(options) {
+  const browserOptions = {
     headless: false,
     defaultViewport: {
-      width: args.width,
-      height: args.height,
-    },
-  });
+      width: options.width,
+      height: options.height,
+    }
+  };
+
+  if (options.fullscreen) {
+    browserOptions.defaultViewport = null;
+    browserOptions.options = ['--start-fullscreen'];
+  }
+
+  return puppeteer.launch(browserOptions);
+}
+
+async function main() {
+  const args = parseArguments();
+  let statusCode = 0;
+
+  const browser = await setupBrowser(args);
   const page = (await browser.pages())[0];
 
   await login(page, args.loginURL, args.username, args.password);
   await page.goto(args.redirect);
-  await browser.close();
+
+  try {
+    await hangUntilStreamEnd(page);
+
+  } catch (err) {
+    console.error(err);
+    statusCode = 1;
+
+  } finally {
+    await browser.close();
+    return statusCode;
+  }
 }
 
 main();
